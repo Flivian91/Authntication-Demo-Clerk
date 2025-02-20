507b62e588118f0ac6be88e4f2cb1280b49ef919
Flivian91 <fliviandev@gmail.com>
2025-02-20 12:28:25 +0300

    commit

--- src/app/components/Navigation.jsx
+++ src/app/components/Navigation.jsx
@@ -8,10 +8,9 @@ import {
   SignOutButton,
 } from "@clerk/nextjs";
 import Link from "next/link";
-import { currentUser } from "@clerk/nextjs/server";
+
 
 const Navigation = async () => {
-  const { imageUrl } = await currentUser();
   return (
     <nav className="flex justify-between items-center p-4 bg-gray-100 text-black">
       <div className="flex items-center">
@@ -29,11 +28,7 @@ const Navigation = async () => {
         </SignedOut>
         <SignedIn>
           <Link role="button" href={"/user-profile"} className="h-10 w-10 flex items-center justify-center bg-gray-200 rounded-full">
-            <img
-              src={imageUrl}
-              alt="User image"
-              className="object-fill h-8 w-8 rounded-full"
-            />
+           <p>profile</p>
           </Link>
           <SignOutButton />
         </SignedIn>
